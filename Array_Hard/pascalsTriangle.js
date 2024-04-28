a7db28a8a4df3ff167b8a751c0dea2fe6e12f15b
Shwetha <107784718+shwethaj1104@users.noreply.github.com>
2024-04-28 08:34:29 +0400

    pascal's triangle problem 2

--- Array_Hard/pascalsTriangle.js
+++ Array_Hard/pascalsTriangle.js
@@ -14,7 +14,7 @@
 
 let row=5
 let column=3
-//output is 3
+//output is 6
 
 // brute force approach, where n=row, r=column
 // n-1             n!
@@ -37,9 +37,51 @@ const findElement=(row,column)=>{
     }
     return res
 }
-// console.log("element in pascal's triangle for given row and column is",findElement(5,1))
+// console.log("element in pascal's triangle for given row and column is",findElement(row-1,column-1))
 
 
 // PROBLEM 2:given the row number print the entire row
 
+//brute force approach
+//we know that every element can be found with nCr formula, and a row will have row number of columns, for 5 rows there will be 5 columns
+//TC=O(row*column)
+//SC-O(1)
+const getEachElement=(row,column)=>{
+    let res=1
+    for(let i=0;i<column;i++){
+        res=res*(row-i)
+        res=res/(i+1)
+    }
+    return res
+}
+const findEntirerow=(row)=>{
+    for(let i=1;i<=row;i++){
+        console.log(getEachElement(row-1,i-1))
+    }
+}
+// console.log("pascal's traingle row for given row is",findEntirerow(row))
 
+//OPTIMAL APPROACH, row=5
+// output =    1       4      6       4         1
+// index       0       1      2       3         4
+// nCr result= 1      1*4    1*4*3   1*4*3*2  1*4*3*2*1
+//                   -----   ------  -------  --------
+//                     1      1*2     1*2*3    1*2*3*4
+
+//that is res *  (row-i)
+//               -------
+//                  i
+
+//TC=0(row)
+//SC=O(1)
+const optimalFindRow=(row)=>{
+    let res=1
+    for(let i=1;i<=row;i++){
+        console.log("pascal's traimgle elements",res)
+        res=res*(row-i)/i
+    }
+}
+// console.log("pascal's traingle row for given row is",optimalFindRow(row))
+
+
+//PROBLEM 3: Print entire pascal's triangle for given n or rows
